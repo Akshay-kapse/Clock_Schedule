c8a93c2815905ff57dd6c72f356b416a08006fcd
Akshay <akshaykapsse@gmail.com>
2025-06-19 10:20:23 +0530

    Initial Commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/', // ✅ This is important for Vercel + React Router
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Let Vite listen on all network interfaces
-    port: 5173, // Optional: Use your desired port
+    host: '0.0.0.0',
+    port: 5173,
   },
 })
