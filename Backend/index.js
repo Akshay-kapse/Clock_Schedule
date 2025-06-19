d2e47941a182076c97a8ea0156ad02e4e70dea89
Akshay <akshaykapsse@gmail.com>
2025-06-19 15:08:39 +0530

    Initial Commit

--- Backend/index.js
+++ Backend/index.js
@@ -23,7 +23,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://right.vercel.app", 
+      "https://right.vercel.app",
+       "https://right-xxxxx-akshays-projects-xxxxx.vercel.app" 
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
