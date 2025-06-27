87e85ac4b484f033f87319b47c78471b21942406
Akshay <akshaykapsse@gmail.com>
2025-06-27 11:38:11 +0530

    Initial Commit

--- Frontend/src/components/AdBanner.jsx
+++ Frontend/src/components/AdBanner.jsx
@@ -1,23 +1,24 @@
 import React, { useEffect } from "react";
 
-function AdBanner() {
+const AdBanner = () => {
   useEffect(() => {
     try {
       (window.adsbygoogle = window.adsbygoogle || []).push({});
     } catch (e) {
-      console.error("AdSense error:", e);
+      console.error("Adsense error:", e);
     }
   }, []);
 
   return (
-    <ins className="adsbygoogle"
-      style={{ display: "block", textAlign: "center" }}
+    <ins
+      className="adsbygoogle"
+      style={{ display: "block" }}
       data-ad-client="ca-pub-8362591593697106"
-      data-ad-slot="7894561230"  
+      data-ad-slot="1173951994"  
       data-ad-format="auto"
-      data-full-width-responsive="true">
-    </ins>
+      data-full-width-responsive="true"
+    ></ins>
   );
-}
+};
 
 export default AdBanner;
